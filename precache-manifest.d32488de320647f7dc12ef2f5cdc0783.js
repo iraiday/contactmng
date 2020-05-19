983c34c27df91487c648ddc17c37d797003c7836
iraiday <65047000+iraiday@users.noreply.github.com>
2020-05-18 23:49:04 -0500

    Updates

---
+++ precache-manifest.d32488de320647f7dc12ef2f5cdc0783.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bde06647b58b8316721c8794204c3c9",
+    "url": "/contactmng/index.html"
+  },
+  {
+    "revision": "d8c821b6ebb29eea8642",
+    "url": "/contactmng/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "db8bce5b22d491f067c8",
+    "url": "/contactmng/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d8c821b6ebb29eea8642",
+    "url": "/contactmng/static/js/2.88463a75.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmng/static/js/2.88463a75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db8bce5b22d491f067c8",
+    "url": "/contactmng/static/js/main.0fe2f6ed.chunk.js"
+  },
+  {
+    "revision": "f0b5250dc28676e33224",
+    "url": "/contactmng/static/js/runtime-main.22592344.js"
+  }
+]);
