30ba1373bb66e8895513d6fa530001e95dc51add
iraiday <65047000+iraiday@users.noreply.github.com>
2020-05-19 00:34:32 -0500

    Update Header.js
    ok

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -6,7 +6,6 @@ function Header(props) {
     <nav className="navbar navbar-dark navbar-expand-sm bg-danger mb-3 py-0">
       <div className="container">
         <Link className="navbar-brand" to="/">
-          <i className="fas fa-plus"></i>
           {props.branding}
         </Link>
 
